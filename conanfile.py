56e438bfe19ac328160f60ed4349678c91af8307
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2020-07-04 23:10:17 +0200

    wip

--- conanfile.py
+++ conanfile.py
@@ -19,7 +19,7 @@ class NetcdfConan(ConanFile):
         "shared": [True, False],
         "fPIC": [True, False],
         "build_utilities": [True, False],
-        "netcdf_4": [True, False],
+        "netcdf4": [True, False],
         "hdf4": [True, False],
         "hdf5": [True, False],
         "dap": [True, False],
@@ -29,7 +29,7 @@ class NetcdfConan(ConanFile):
         "shared": False,
         "fPIC": True,
         "build_utilities": True,
-        "netcdf_4": True,
+        "netcdf4": True,
         "hdf4": False,
         "hdf5": True,
         "dap": True,
@@ -51,28 +51,34 @@ class NetcdfConan(ConanFile):
             del self.options.fPIC
 
     def configure(self):
+        if self.options.shared:
+            del self.options.fPIC
         del self.settings.compiler.cppstd
         del self.settings.compiler.libcxx
-        if not self.options.netcdf_4:
-            del self.options.hdf4
-            del self.options.hdf5
-        if not (self.options.netcdf_4 and self.options.hdf5):
-            del self.options.parallel
-        if self.options.get_safe("hdf5", False):
+
+        if self.options.netcdf4 and not self.options.hdf5:
+            raise ConanInvalidConfiguration("netcdf4 requires hdf5")
+        if not self.options.netcdf4:
+            if self.options.hdf4:
+                raise ConanInvalidConfiguration("netcdf4 is required for hdf4 features")
+            if self.options.parallel:
+                raise ConanInvalidConfiguration("netcdf4 is required for parallel IO")
+
+        if self.options.hdf5:
             self.options["hdf5"].with_zlib = True
             self.options["hdf5"].hl = True
-        if self.options.get_safe("parallel", False):
+        if self.options.parallel:
             raise ConanInvalidConfiguration("parallel option requires openmpi which is not yet available in CCI")
             self.options["hdf5"].parallel = True # TODO: option not yet available in hdf5 recipe, requires openmpi in CCI
 
     def requirements(self):
-        if self.options.get_safe("hdf4", False):
+        if self.options.hdf4:
             self.requires("hdf4/4.2.15")
-        if self.options.get_safe("hdf5", False):
+        if self.options.hdf5:
             self.requires("hdf5/1.12.0")
         if self.options.dap:
             self.requires("libcurl/7.70.0")
-        if self.options.get_safe("parallel", False):
+        if self.options.parallel:
             self.requires("openmpi/4.0.3")
 
     def source(self):
@@ -88,54 +94,55 @@ class NetcdfConan(ConanFile):
     def _configure_cmake(self):
         if self._cmake:
             return self._cmake
-        self._cmake = CMake(self)
+        cmake = CMake(self)
         if self.settings.compiler == "Visual Studio":
-            self._cmake.definitions["NC_USE_STATIC_CRT"] = str(self.settings.compiler.runtime).startswith("MT")
-        self._cmake.definitions["ENABLE_V2_API"] = True
-        self._cmake.definitions["BUILD_UTILITIES"] = self.options.build_utilities
-        self._cmake.definitions["ENABLE_MMAP"] = True
-        self._cmake.definitions["ENABLE_EXAMPLES"] = False
-        self._cmake.definitions["ENABLE_HDF4"] = self.options.get_safe("hdf4", False)
-        if self.options.get_safe("hdf4", False):
-            self._cmake.definitions["ENABLE_HDF4_FILE_TESTS"] = False
-        self._cmake.definitions["ENABLE_NETCDF_4"] = self.options.netcdf_4
-        self._cmake.definitions["ENABLE_LOGGING"] = False
-        self._cmake.definitions["ENABLE_SET_LOG_LEVEL_FUNC"] = True
-        self._cmake.definitions["ENABLE_STRICT_NULL_BYTE_HEADER_PADDING"] = False
-        self._cmake.definitions["ENABLE_RPC"] = False
-        self._cmake.definitions["USE_HDF5"] = self.options.get_safe("hdf5", False)
-        if self.options.get_safe("hdf5", False):
-            self._cmake.definitions["NC_ENABLE_HDF_16_API"] = True
-        self._cmake.definitions["ENABLE_DAP"] = self.options.dap
-        self._cmake.definitions["ENABLE_BYTERANGE"] = False
-        self._cmake.definitions["ENABLE_DAP_LONG_TESTS"] = False
-        self._cmake.definitions["ENABLE_DAP_REMOTE_TESTS"] = False
-        self._cmake.definitions["ENABLE_EXTRA_TESTS"] = False
+            cmake.definitions["NC_USE_STATIC_CRT"] = str(self.settings.compiler.runtime).startswith("MT")
+        cmake.definitions["ENABLE_V2_API"] = True
+        cmake.definitions["BUILD_UTILITIES"] = self.options.build_utilities
+        cmake.definitions["ENABLE_MMAP"] = True
+        cmake.definitions["ENABLE_EXAMPLES"] = False
+        cmake.definitions["ENABLE_HDF4"] = self.options.hdf4
+        if self.options.hdf4:
+            cmake.definitions["ENABLE_HDF4_FILE_TESTS"] = False
+        cmake.definitions["ENABLE_NETCDF_4"] = self.options.netcdf4
+        cmake.definitions["ENABLE_LOGGING"] = False
+        cmake.definitions["ENABLE_SET_LOG_LEVEL_FUNC"] = True
+        cmake.definitions["ENABLE_STRICT_NULL_BYTE_HEADER_PADDING"] = False
+        cmake.definitions["ENABLE_RPC"] = False
+        cmake.definitions["USE_HDF5"] = self.options.hdf5
+        if self.options.hdf5:
+            cmake.definitions["NC_ENABLE_HDF_16_API"] = True
+        cmake.definitions["ENABLE_DAP"] = self.options.dap
+        cmake.definitions["ENABLE_BYTERANGE"] = False
+        cmake.definitions["ENABLE_DAP_LONG_TESTS"] = False
+        cmake.definitions["ENABLE_DAP_REMOTE_TESTS"] = False
+        cmake.definitions["ENABLE_EXTRA_TESTS"] = False
         if self.settings.compiler == "Visual Studio":
-            self._cmake.definitions["ENABLE_XGETOPT"] = True
+            cmake.definitions["ENABLE_XGETOPT"] = True
         if self.settings.os != "Windows":
-            self._cmake.definitions["ENABLE_STDIO"] = False
-            self._cmake.definitions["ENABLE_FFIO"] = False
-        self._cmake.definitions["ENABLE_TESTS"] = False
-        self._cmake.definitions["ENABLE_EXTREME_NUMBERS"] = False
-        self._cmake.definitions["ENABLE_METADATA_PERF_TESTS"] = False
-        self._cmake.definitions["ENABLE_FSYNC"] = False # Option?
-        self._cmake.definitions["ENABLE_JNA"] = False
-        self._cmake.definitions["ENABLE_LARGE_FILE_SUPPORT"] = True # Option?
-        self._cmake.definitions["ENABLE_EXAMPLE_TESTS"] = False
-        self._cmake.definitions["ENABLE_PARALLEL4"] = self.options.get_safe("parallel", False)
-        self._cmake.definitions["ENABLE_PNETCDF"] = False
-        self._cmake.definitions["ENABLE_ERANGE_FILL"] = False
-        self._cmake.definitions["ENABLE_PARALLEL_TESTS"] = False
-        self._cmake.definitions["ENABLE_FILTER_TESTING"] = False
-        self._cmake.definitions["ENABLE_CLIENTSIDE_FILTERS"] = False
-        self._cmake.definitions["ENABLE_DOXYGEN"] = False
-        self._cmake.definitions["ENABLE_DISKLESS"] = True
+            cmake.definitions["ENABLE_STDIO"] = False
+            cmake.definitions["ENABLE_FFIO"] = False
+        cmake.definitions["ENABLE_TESTS"] = False
+        cmake.definitions["ENABLE_EXTREME_NUMBERS"] = False
+        cmake.definitions["ENABLE_METADATA_PERF_TESTS"] = False
+        cmake.definitions["ENABLE_FSYNC"] = False # Option?
+        cmake.definitions["ENABLE_JNA"] = False
+        cmake.definitions["ENABLE_LARGE_FILE_SUPPORT"] = True # Option?
+        cmake.definitions["ENABLE_EXAMPLE_TESTS"] = False
+        cmake.definitions["ENABLE_PARALLEL4"] = self.options.parallel
+        cmake.definitions["ENABLE_PNETCDF"] = False
+        cmake.definitions["ENABLE_ERANGE_FILL"] = False
+        cmake.definitions["ENABLE_PARALLEL_TESTS"] = False
+        cmake.definitions["ENABLE_FILTER_TESTING"] = False
+        cmake.definitions["ENABLE_CLIENTSIDE_FILTERS"] = False
+        cmake.definitions["ENABLE_DOXYGEN"] = False
+        cmake.definitions["ENABLE_DISKLESS"] = True
         if self.settings.compiler == "Visual Studio":
-            self._cmake.definitions["NC_MSVC_STACK_SIZE"] = 40000000 # Option?
-        self._cmake.definitions["ENABLE_CDF5"] = True
-        self._cmake.definitions["ENABLE_BASH_SCRIPT_TESTING"] = False
-        self._cmake.configure(build_folder=self._build_subfolder)
+            cmake.definitions["NC_MSVC_STACK_SIZE"] = 40000000 # Option?
+        cmake.definitions["ENABLE_CDF5"] = True
+        cmake.definitions["ENABLE_BASH_SCRIPT_TESTING"] = False
+        cmake.configure(build_folder=self._build_subfolder)
+        self._cmake = cmake
         return self._cmake
 
     def package(self):
@@ -155,13 +162,13 @@ class NetcdfConan(ConanFile):
         self.cpp_info.components["netcdf"].names["cmake_find_package"] = "netcdf"
         self.cpp_info.components["netcdf"].names["cmake_find_package_multi"] = "netcdf"
         self.cpp_info.components["netcdf"].libs = tools.collect_libs(self)
-        if self.options.get_safe("hdf4", False):
+        if self.options.hdf4:
             self.cpp_info.components["netcdf"].requires.append("hdf4::hdf4")
-        if self.options.get_safe("hdf5", False):
+        if self.options.hdf5:
             self.cpp_info.components["netcdf"].requires.append("hdf5::hdf5") # TODO: when components available in hdf5 recipe => requires.extend([hdf5::hl, hdf5::c])
         if self.options.dap:
             self.cpp_info.components["netcdf"].requires.append("libcurl::libcurl")
-        if self.options.get_safe("parallel", False):
+        if self.options.parallel:
             self.cpp_info.components["netcdf"].requires.append("openmpi::openmpi")
         if self.settings.os == "Linux":
             self.cpp_info.components["netcdf"].system_libs.append("m")
